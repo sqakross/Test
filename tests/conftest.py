fa3b17754105718e2e9f5c59f2901f7dca83bbb3
sqakross <sqakross@gmail.com>
2024-08-14 00:52:24 -0700

    update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,9 @@ def driver():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--incognito")
     chrome_options.add_argument("--window-size=1440,1080")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
     service = Service(executable_path=ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
     yield driver
